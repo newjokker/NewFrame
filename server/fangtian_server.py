7d10ce52155e63dfe4392928eae93f2c8222bb09
jokker <18761609908@163.com>
2021-10-27 09:40:29 +0800

    * 初步完善 fangtian_server.py

--- server/fangtian_server.py
+++ server/fangtian_server.py
@@ -5,57 +5,67 @@
 * 定时扫 xml 文件夹，并据此生成 log 和 csv
 """
 
-import subprocess
 import os
 import time
 import shutil
 import argparse
 from JoTools.utils.FileOperationUtil import FileOperationUtil
 from JoTools.txkjRes.deteRes import DeteRes
-from JoTools.utils.CsvUtil import CsvUtil
 from .core.save_log import SaveLog
 
 
 class FTserver(object):
 
-    def __init__(self, img_dir, xml_dir, res_dir, sign_dir):
+    def __init__(self, img_dir, xml_dir, res_dir, sign_dir, mul_progress_num):
         self.img_dir = img_dir
         self.xml_dir = xml_dir
         self.res_dir = res_dir
         self.sign_dir = sign_dir
-        #
-        os.makedirs(res_dir, exist_ok=True)
-        os.makedirs(sign_dir, exist_ok=True)
-        os.makedirs(xml_dir, exist_ok=True)
+        self.mul_progress_num = mul_progress_num            # 进程个数
         #
         self.start_time = time.time()
         self.stop_time = None
         self.max_dete_time = -1
-        self.dete_img_index = 0     # 已检测的图片的数量
+        self.dete_img_index = 0                             # 已检测的图片的数量
         #
         log_path = os.path.join(res_dir, 'log')
         csv_path = os.path.join(res_dir, 'result.csv')
         img_count = len(list(FileOperationUtil.re_all_file(self.img_dir, endswitch=['.jpg', '.JPG', '.png', '.PNG'])))
         self.all_img_count = img_count                                                           # 所有要检测图片的数目
         self.save_log = SaveLog(log_path, img_count, csv_path)
+        self.max_dete_time = 9.5 * self.all_img_count
 
     def if_end(self):
         """根据 sign 文件夹中的信息，判断是否已经结束"""
+        for i in range(1, self.mul_progress_num+1):
+            each_txt_path = os.path.join(self.sign_dir, "{0}.txt".format(i))
+            if not os.path.exists(each_txt_path):
+                return False
+        return True
 
-        def check_res_file_num(res_txt_dir, txt_num):
-            for i in range(1, txt_num + 1):
-                each_txt_path = os.path.join(res_txt_dir, "{0}.txt".format(i))
-                if not os.path.exists(each_txt_path):
-                    return False
-            return True
-
-    def get_max_dete_time(self):
+    def get_max_dete_time(self, assign_each_dete_use_time=9.5):
         """获取最长的检测时间，超过检测时间自动退出并保存日志和 csv"""
+        self.max_dete_time = assign_each_dete_use_time * self.all_img_count
+
+    def empty_history_info(self):
+        """清空历史数据"""
+
+        if os.path.exists(self.res_dir):
+            shutil.rmtree(self.res_dir)
+
+        if os.path.exists(self.sign_dir):
+            shutil.rmtree(self.sign_dir)
+
+        if os.path.exists(self.xml_dir):
+            shutil.rmtree(self.xml_dir)
+
+        os.makedirs(self.res_dir, exist_ok=True)
+        os.makedirs(sign_dir, exist_ok=True)
+        os.makedirs(xml_dir, exist_ok=True)
 
     def start_monitor(self):
         """开始监听"""
         img_name_dict = {}
-        self.dete_img_index = 1
         while True:
             # dete end
             if self.save_log.img_index > self.save_log.img_count:
@@ -63,16 +73,16 @@ class FTserver(object):
                 return
 
             # all end
-            elif check_res_file_num(res_txt_dir, mul_process_num):
+            elif self.if_end():
                 self.stop_time = time.time()
                 return
 
             xml_path_list = FileOperationUtil.re_all_file(res_dir, endswitch=['.xml'])
 
-            time.sleep(5)
+            time.sleep(3)
 
             for each_xml_path in xml_path_list:
-                print("* {0} {1}".format(self.dete_img_index, each_xml_path))
+                print("* {0} {1}".format(self.dete_img_index + 1, each_xml_path))
                 img_name = img_name_dict[FileOperationUtil.bang_path(each_xml_path)[1]]
                 try:
                     # wait for write end
@@ -95,6 +105,13 @@ class FTserver(object):
         use_time = self.stop_time - self.start_time
         print("* check img {0} use time {1} {2} s/pic".format(self.all_img_count, use_time, use_time / self.all_img_count))
 
+    def main(self):
+        """主流程"""
+        self.empty_history_info()
+        self.get_max_dete_time(9.5)
+        self.start_monitor()
+        self.get_csv()
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Tensorflow Faster R-CNN demo')
@@ -103,12 +120,7 @@ def parse_args():
     parser.add_argument('--xml_dir',dest='xml_dir',type=str, default=r"/usr/input_picture")
     parser.add_argument('--res_dir',dest='res_dir',type=str, default=r"/usr/input_picture")
     parser.add_argument('--sign_dir',dest='rsign_dir',type=str, default=r"/usr/input_picture")
-    #
-    parser.add_argument('--modelList',dest='modelList',default="M1,M2,M3,M4,M5,M6,M7,M8,M9")
-    parser.add_argument('--jsonPath',dest='jsonPath', default=r"/usr/input_picture_attach/pictureName.json")
-    parser.add_argument('--outputDir',dest='outputDir', default=r"/usr/output_dir")
-    #
-    parser.add_argument('--scriptIndex',dest='scriptIndex', default=r"1-1")
+    parser.add_argument('--mul_progress_num',dest='mul_progress_num',type=str, default=1)
     #
     args = parser.parse_args()
     return args
@@ -117,8 +129,8 @@ def parse_args():
 if __name__ == "__main__":
 
     args = parse_args()
-    ft_server = FTserver(args.img_dir, args.xml_dir, args.res_dir, args.sign_dir)
-
+    ft_server = FTserver(args.img_dir, args.xml_dir, args.res_dir, args.sign_dir, args.mul_progress_num)
+    ft_server.main()
 
 
 
