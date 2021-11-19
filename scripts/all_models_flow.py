54d55bdc9620f0305d309ea9fb6acc60332bbd50
jokker <18761609908@163.com>
2021-11-19 09:53:37 +0800

    * 默认使用的模型去除 xjQX jyhQX

--- scripts/all_models_flow.py
+++ scripts/all_models_flow.py
@@ -263,7 +263,7 @@ if __name__ == '__main__':
     scriptName = os.path.basename(__file__).split('.')[0]
     #
     # all_model_list = ['nc', 'jyzZB', 'fzc', 'fzcRust', 'kkxTC', 'kkxQuiting', 'xjQX', 'jyhQX', 'kkxClearance']
-    all_model_list = ['nc', 'jyzZB', 'fzc', 'fzcRust', 'kkxTC', 'kkxQuiting']
+    all_model_list = ['nc', 'jyzZB', 'fzc', 'fzcRust', 'kkxTC', 'kkxQuiting', 'kkxClearance']
     # all_model_list = ['xjQX', 'jyhQX']
 
     all_model_dict = all_model_restore(args, scriptName, all_model_list)
