4649e0f4b6114969002bfcd06c1500cb13456d87
jokker <18761609908@163.com>
2022-01-07 11:09:00 +0800

    * 泰州 allflow，规范输入

--- scripts/allflow_taizhou.py
+++ scripts/allflow_taizhou.py
@@ -7,8 +7,6 @@ import time
 import shutil
 import argparse
 import configparser
-from JoTools.utils.FileOperationUtil import FileOperationUtil
-from JoTools.txkjRes.deteRes import DeteRes
 import base64
 import prettytable
 import sys
@@ -17,7 +15,6 @@ import numpy as np
 import cv2
 from gevent import monkey
 from gevent.pywsgi import WSGIServer
-import datetime
 #
 from JoTools.txkjRes.deteRes import DeteRes
 from JoTools.utils.FileOperationUtil import FileOperationUtil
@@ -31,28 +28,28 @@ app = Flask(__name__)
 
 # todo 鸟巢，裸导线（最新的）
 
+# todo 这边有新的任务就会不断地去起，要不要这边判断 GPU 资源是否够 再去决定是否起来？
 
-# @app.route('/dete', methods=['post'])
+
+@app.route('/dete', methods=['post'])
 def start_dete():
     """获取检测状态"""
 
     try:
-
-        # model_list = request.form['model_list']
-        # img_path_list = request.form['img_path_list']
-        # post_usr = request.form['post_url']
-        model_list = ['nc']
-
-        # img_path_list = [r"/home/ldq/input_dir/JPEGImages/0a6509ccee8414f107d7af90a8fe8017.jpg",
-        #                  r"/home/ldq/input_dir/JPEGImages/0b03f77cd53aad32d5019872ae034e9c.jpg"]
-
-        img_path_list = list(FileOperationUtil.re_all_file(r"/home/ldq/input_dir/JPEGImages", endswitch=['.jpg', '.JPG', '.png', '.PNG']))
-
-
-        post_usr = r"192.168.3.101:2020/test"
-        batch_id = "no_batch_id"
+        model_list = request.form['model_list'].split(',')
+        img_path_list = request.form['img_path_list'].split(',')
+        post_url = request.form['post_url']
+        batch_id = request.form['batch_id']
         #
-        assign_save_dir = os.path.join(save_dir, str(uuid.uuid1()))
+
+        print('-'*100)
+        print("* model list : {0}, type : {1}".format(model_list, type(model_list)))
+        print("* img path list length: {0}, type : {1}".format(len(img_path_list), type(img_path_list)))
+        print("* post url : {0}, type : {1}".format(post_url, type(post_url)))
+        print("* batch id : {0}, type : {1}".format(batch_id, type(batch_id)))
+        print('-'*100)
+
+        assign_save_dir = os.path.join(save_dir, batch_id+ "-" + str(uuid.uuid1()))
         os.makedirs(assign_save_dir, exist_ok=True)
         xml_tmp_dir = os.path.join(assign_save_dir, "xml_tmp")
         os.makedirs(xml_tmp_dir, exist_ok=True)
@@ -75,7 +72,7 @@ def start_dete():
         taizhou_server_bug_file = open(os.path.join(log_dir, "taizhou_server_bug" + str(time.time())[:10] + "_allflow.txt"), "w+")
         taizhou_server_std_file = open(os.path.join(log_dir, "taizhou_server_std" + str(time.time())[:10] + "_allflow.txt"), "w+")
         server_cmd_str = r"python3 scripts/server/taizhou/taizhou_server.py --xml_tmp {0} --xml_res {1} --post_url {2} --img_count {3} --mul_progress_num {4} --sign_end_txt_dir {5} --print_process {6} --batch_id {7}".format(
-            xml_tmp_dir, xml_res_dir, post_usr, img_count, mul_process_num, sign_dir, "True", batch_id)
+            xml_tmp_dir, xml_res_dir, post_url, img_count, mul_process_num, sign_dir, "True", batch_id)
         pid = subprocess.Popen(server_cmd_str.split(), stdout=taizhou_server_std_file, stderr=taizhou_server_bug_file, shell=False)
         print("* server pid : {0}, cmd str : {1}".format(pid, server_cmd_str))
         # --------------------------------------------------------------------------------------------------------------
@@ -90,10 +87,12 @@ def start_dete():
             print("* {0}".format(each_cmd_str))
             time.sleep(1)
 
-    #     return jsonify({"status":"OK"})
+        return jsonify({"status":"OK"})
     except Exception as e:
         print(e)
-        # return jsonify({"status": "ERROR:{0}".format(e)})
+        print(e.__traceback__.tb_frame.f_globals["__file__"])
+        print(e.__traceback__.tb_lineno)
+        return jsonify({"status": "ERROR:{0}".format(e)})
 
 
 def serv_start():
@@ -106,11 +105,9 @@ def parse_args():
     parser.add_argument('--port', dest='port', type=int, default=3232)
     parser.add_argument('--host', dest='host', type=str, default='0.0.0.0')
     #
-    parser.add_argument('--model_list', dest='model_list', type=str)
     parser.add_argument('--save_dir', dest='save_dir', type=str)
     parser.add_argument('--mul_process_num', dest='mul_process_num', type=int, default=2)
-    #
-    parser.add_argument('--gpu_id_list', dest='gpu_id_list', type=int, default="0")
+    parser.add_argument('--gpu_id_list', dest='gpu_id_list', type=str, default="0")
     #
     args = parser.parse_args()
     return args
@@ -122,17 +119,10 @@ if __name__ == "__main__":
     portNum = args.port
     host = args.host
     #
-    # save_dir = args.save_dir
-    # model_list = args.model_list
-    # mul_process_num = args.mul_process_num
-    # gpu_id_list = args.gpu_id_list.split(",")
+    save_dir = args.save_dir
+    mul_process_num = args.mul_process_num
+    gpu_id_list = args.gpu_id_list.split(",")
     #
-    save_dir = r"/home/ldq/NewFrame_TaiZhou/tmpfiles"
-    model_list = ['nc']
-    mul_process_num = 2
-    gpu_id_list = ['0']
-    #
-
-    start_dete()
+    serv_start()
 
 
